38a936ecc1d220721ae5cdf580c297318e52b4fd
SnowIsWhite <jibae617@gmail.com>
2019-05-13 21:37:57 +0900

    bigram selector

--- core/word2vec.py
+++ core/word2vec.py
@@ -3,11 +3,14 @@ import json
 import ast
 import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
- 
+
+selector = 0
 trees_path = './tokenized_data.json'
+trees_path_bigram = './tokenized_data_bigram.json'
 wv_path = './wv_model/vectors.kv'
 # list of list of tokens
 
+if selector == 1: trees_path = trees_path_bigram
 with open(trees_path, 'r') as f:
     lines = f.readline()
 sentences = ast.literal_eval(lines)
